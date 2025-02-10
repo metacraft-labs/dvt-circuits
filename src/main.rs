957e814cbd94b19340a4a31ce232bce4a245b2af
Marto <martindobrev0@gmail.com>
2025-02-10 20:04:37 +0200

    Fix review comments

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use std::env;
 
 use jsonschema::JSONSchema;
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
 
 pub const SHARE_PROVER_ELF: &[u8] = include_elf!("share_exchange_prove");
 pub const FINALE_PROVER_ELF: &[u8] = include_elf!("finalization_prove");
@@ -47,14 +45,6 @@ struct Cli {
     mode: Mode,
 }
 
-fn read_text_file(filename: &str) -> Result<String, Box<dyn Error>> {
-    let mut file = File::open(filename).map_err(|e| format!("Error opening file: {}", e))?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .map_err(|e| format!("Error reading file: {}", e))?;
-    Ok(contents)
-}
-
 fn execute<T>(data: &T, elf: &[u8], show_report: bool)
 where
     T: ProverSerialize,
@@ -63,10 +53,10 @@ where
     let mut stdin = SP1Stdin::new();
     data.write(&mut stdin);
     let client = ProverClient::new();
-    let (_public_values, report) = client.execute(elf, stdin).run().unwrap_or_else(|e| {
-        eprintln!("Failed to prove: {}", e);
-        std::process::exit(1);
-    });
+    let (_public_values, report) = client
+        .execute(elf, stdin)
+        .run()
+        .unwrap_or_else(|e| panic!("Failed to prove: {}", e));
 
     print!("executed");
     if show_report {
@@ -84,20 +74,16 @@ where
 
     let (pk, _) = client.setup(elf);
 
-    print!("proving....");
     let proof = client.prove(&pk, stdin).run().unwrap_or_else(|e| {
-        eprintln!("Failed to prove: {}", e);
-        std::process::exit(1);
+        panic!("Failed to prove: {}", e);
     });
 
-    print!("saving file");
     proof.save("proof.bin").unwrap();
-    print!("file was saved")
 }
 
 fn validate_json(schema_path: &str, json_path: &str) -> Result<(), Box<dyn Error>> {
-    let schema = read_text_file(schema_path)?;
-    let json = read_text_file(json_path)?;
+    let schema = dvt_abi::read_text_file(schema_path)?;
+    let json = dvt_abi::read_text_file(json_path)?;
 
     let schema = serde_json::from_str(&schema).unwrap();
     let data = serde_json::from_str(&json).unwrap();
@@ -124,35 +110,27 @@ fn validate_json(schema_path: &str, json_path: &str) -> Result<(), Box<dyn Error
 fn main() {
     let args = Cli::parse();
 
-    if args.input_file.is_empty() {
-        eprintln!("No file name provided after '--input-file'.");
-        std::process::exit(1);
-    }
-
-    if args.json_schema.is_some() {
-        let ok = validate_json(args.json_schema.unwrap().as_str(), args.input_file.as_str());
+    if let Some(path_to_schema) = args.json_schema {
+        let ok = validate_json(path_to_schema.as_str(), args.input_file.as_str());
         if ok.is_err() {
-            eprintln!("{}", ok.unwrap_err());
-            std::process::exit(1);
+            panic!("{}", ok.unwrap_err());
         }
     }
 
     utils::setup_logger();
 
     // Depending on the command type, we could do different things here.
-    // For now, both variants handle the data similarly.
+    // For now, all variants handle the data similarly.
     match args.command_type {
         CommandType::Share => {
             let data =
                 dvt_abi::read_data_from_json_file::<dvt_abi::DvtBlsSharedData>(&args.input_file)
                     .unwrap_or_else(|e| {
-                        eprintln!("Error parsing JSON: {}", e);
-                        std::process::exit(1);
+                        panic!("Error parsing JSON: {}", e);
                     });
 
             let abi_data = data.to_abi().unwrap_or_else(|e| {
-                eprintln!("Error converting to ABI data: {}", e);
-                std::process::exit(1);
+                panic!("Error converting to ABI data: {}", e);
             });
             match args.mode {
                 Mode::Prove => {
@@ -167,13 +145,11 @@ fn main() {
             let data =
                 dvt_abi::read_data_from_json_file::<dvt_abi::DvtFinalizationData>(&args.input_file)
                     .unwrap_or_else(|e| {
-                        eprintln!("Error parsing JSON: {}", e);
-                        std::process::exit(1);
+                        panic!("Error parsing JSON: {}", e);
                     });
 
             let abi_data = data.to_abi().unwrap_or_else(|e| {
-                eprintln!("Error converting to ABI data: {}", e);
-                std::process::exit(1);
+                panic!("Error converting to ABI data: {}", e);
             });
 
             match args.mode {
@@ -190,13 +166,11 @@ fn main() {
                 &args.input_file,
             )
             .unwrap_or_else(|e| {
-                eprintln!("Error parsing JSON: {}", e);
-                std::process::exit(1);
+                panic!("Error parsing JSON: {}", e);
             });
 
             let abi_data = data.to_abi().unwrap_or_else(|e| {
-                eprintln!("Error converting to ABI data: {}", e);
-                std::process::exit(1);
+                panic!("Error converting to ABI data: {}", e);
             });
 
             match args.mode {
@@ -217,13 +191,11 @@ fn main() {
                 &args.input_file,
             )
             .unwrap_or_else(|e| {
-                eprintln!("Error parsing JSON: {}", e);
-                std::process::exit(1);
+                panic!("Error parsing JSON: {}", e);
             });
 
             let abi_data = data.to_abi().unwrap_or_else(|e| {
-                eprintln!("Error converting to ABI data: {}", e);
-                std::process::exit(1);
+                panic!("Error converting to ABI data: {}", e);
             });
 
             match args.mode {
